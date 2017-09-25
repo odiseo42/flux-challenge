1bdc90039ee74eca98b4dcf6b399c79396880a26
Erick Mendoza <contact@diseo.net>
2017-09-25 01:59:39 -0500

    Using redux webtools

--- submissions/odiseo42/src/store/configureStore.js
+++ submissions/odiseo42/src/store/configureStore.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
+const createStoreWithMiddleware = compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
 )(createStore);
 
 export default function configureStore() {
